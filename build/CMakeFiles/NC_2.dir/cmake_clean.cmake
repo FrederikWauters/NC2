d87037ba279b1c82cbcaf2107d27649e7345ea65
FrederikWauters <fwauters@uw.edu>
2017-12-12 17:27:54 +0100

    remove build dir

--- build/CMakeFiles/NC_2.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/NC_2"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/NC_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
